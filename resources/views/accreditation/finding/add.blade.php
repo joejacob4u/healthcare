c46f88a505a9c2eaf14b22514302dd1b60a5ec93
joe kunnummyalil <jkunn@carbonmedia.com>
2018-05-18 17:24:28 -0400

    fix finding issue

--- resources/views/accreditation/finding/add.blade.php
+++ resources/views/accreditation/finding/add.blade.php
@@ -116,7 +116,6 @@
               {!! Form::hidden('status', 'initial') !!}
               {!! Form::hidden('building_id', session('building_id')) !!}
               {!! Form::hidden('accreditation_id', session('accreditation_id')) !!}
-              {!! Form::hidden('healthsystem_id', Auth::guard('system_user')->user()->healthsystem_id) !!}
 
               {!! Form::hidden('created_by_user_id',Auth::guard('system_user')->user()->id) !!}
 
