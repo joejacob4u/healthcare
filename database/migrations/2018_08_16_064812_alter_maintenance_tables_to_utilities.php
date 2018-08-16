2c5e90c4179ffc87e99538e8ee0fbe09671688ed
joe kunnummyalil <jkunn@carbonmedia.com>
2018-08-16 04:23:03 -0400

    changes with foreign key restraints

--- database/migrations/2018_08_16_064812_alter_maintenance_tables_to_utilities.php
+++ database/migrations/2018_08_16_064812_alter_maintenance_tables_to_utilities.php
@@ -15,25 +15,31 @@ class AlterMaintenanceTablesToUtilities extends Migration
     {
         Schema::disableForeignKeyConstraints();
 
-        Schema::table('maintenance_asset_categories', function (Blueprint $table) {
-            $table->renameColumn('maintenance_physical_risk_id', 'utility_physical_risk_id');
-            $table->renameColumn('maintenance_utility_function_id', 'utility_utility_function_id');
-            $table->dropForeign('maintenance_asset_categories_maintenance_category_id_foreign');
-        });
+        // Schema::table('maintenance_asset_categories', function (Blueprint $table) {
+        //     $table->renameColumn('maintenance_physical_risk_id', 'utility_physical_risk_id');
+        //     $table->renameColumn('maintenance_utility_function_id', 'utility_utility_function_id');
+        //     $table->dropForeign('maintenance_asset_categories_maintenance_category_id_foreign');
+        // });
 
         Schema::rename('maintenance_asset_categories', 'utility_asset_categories');
+        Schema::rename('maintenance_categories', 'utility_categories');
+
+        Schema::table('utility_asset_categories', function (Blueprint $table) {
+            $table->renameColumn('maintenance_physical_risk_id', 'utility_physical_risk_id');
+            $table->renameColumn('maintenance_utility_function_id', 'utility_utility_function_id');
+            //$table->dropForeign('maintenance_asset_categories_maintenance_category_id_foreign');
+            $table->renameColumn('maintenance_category_id', 'utility_category_id');
+            $table->foreign('utility_category_id')->references('id')->on('utility_categories')->onDelete('cascade');
+        });
+
 
         Schema::rename('maintenance_asset_category_to_eop', 'utility_asset_category_to_eop');
-        Schema::rename('maintenance_categories', 'utility_categories');
+        
         Schema::rename('maintenance_frequency_requirements', 'utility_frequency_requirements');
         Schema::rename('maintenance_incident_histories', 'utility_incident_histories');
         Schema::rename('maintenance_physical_risks', 'utility_physical_risks');
         Schema::rename('maintenance_utility_functions', 'utility_utility_functions');
 
-        Schema::table('utility_asset_categories', function (Blueprint $table) {
-            $table->renameColumn('maintenance_category_id', 'utility_category_id');
-            $table->foreign('utility_category_id')->references('id')->on('utility_categories')->onDelete('cascade');
-        });
 
 
         Schema::table('utility_asset_category_to_eop', function (Blueprint $table) {
