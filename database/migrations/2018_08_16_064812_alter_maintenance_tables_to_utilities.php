eaa1b69b6fac70a770f1b52040fcad6e7e74ed62
joe kunnummyalil <jkunn@carbonmedia.com>
2018-08-16 03:40:20 -0400

    migration fix

--- database/migrations/2018_08_16_064812_alter_maintenance_tables_to_utilities.php
+++ database/migrations/2018_08_16_064812_alter_maintenance_tables_to_utilities.php
@@ -13,16 +13,15 @@ class AlterMaintenanceTablesToUtilities extends Migration
      */
     public function up()
     {
-        Schema::rename('maintenance_asset_categories', 'utility_asset_categories');
 
-        Schema::table('utility_asset_categories', function (Blueprint $table) {
+        Schema::table('maintenance_asset_categories', function (Blueprint $table) {
             $table->renameColumn('maintenance_physical_risk_id', 'utility_physical_risk_id');
             $table->renameColumn('maintenance_utility_function_id', 'utility_utility_function_id');
             $table->dropForeign('maintenance_category_id');
-            $table->renameColumn('maintenance_category_id', 'utility_category_id');
-            $table->foreign('utility_category_id')->references('id')->on('utility_categories')->onDelete('cascade');
         });
-        
+
+        Schema::rename('maintenance_asset_categories', 'utility_asset_categories');
+
         Schema::rename('maintenance_asset_category_to_eop', 'utility_asset_category_to_eop');
         Schema::rename('maintenance_categories', 'utility_categories');
         Schema::rename('maintenance_frequency_requirements', 'utility_frequency_requirements');
@@ -30,6 +29,12 @@ class AlterMaintenanceTablesToUtilities extends Migration
         Schema::rename('maintenance_physical_risks', 'utility_physical_risks');
         Schema::rename('maintenance_utility_functions', 'utility_utility_functions');
         Schema::drop('maintenance_utility_function');
+
+        Schema::table('utility_asset_categories', function (Blueprint $table) {
+            $table->renameColumn('maintenance_category_id', 'utility_category_id');
+            $table->foreign('utility_category_id')->references('id')->on('utility_categories')->onDelete('cascade');
+        });
+
 
         Schema::table('utility_asset_category_to_eop', function (Blueprint $table) {
             $table->dropForeign('mc_id');
