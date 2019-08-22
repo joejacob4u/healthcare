d45c6f4aa9ea4bf4328db83f66b7eebc4ac4ca80
joe kunnummyalil <jkunn@carbonmedia.com>
2019-08-22 16:18:59 -0400

    huddle fix

--- app/Http/Controllers/Frontend/Huddle/HuddleController.php
+++ app/Http/Controllers/Frontend/Huddle/HuddleController.php
@@ -52,7 +52,7 @@ class HuddleController extends Controller
 
         //calculate all dm ids for that building to fetch ilsm assessments
         $demand_work_orders = DemandWorkOrder::whereIn('building_department_id', $huddle->careTeam->departments->pluck('id'))->pluck('id');
-        $ilsm_assessments = IlsmAssessment::whereIn('demand_work_order_id', $demand_work_orders)->where('created_at', '>=', \Carbon\Carbon::today()->subDays(180))->get();
+        $ilsm_assessments = IlsmAssessment::whereIn('work_order_id', $demand_work_orders)->where('created_at', '>=', \Carbon\Carbon::today()->subDays(180))->get();
         $assessments = Assessment::whereIn('building_department_id', $huddle->careTeam->departments->pluck('id'))->get();
         return view('huddle.user.view', ['huddle' => $huddle, 'users' => $users, 'ilsm_assessments' => $ilsm_assessments, 'assessments' => $assessments]);
     }
