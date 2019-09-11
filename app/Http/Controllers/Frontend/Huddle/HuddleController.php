651b8ce41fac79f81d3817e30c20426f525dafcf
joe kunnummyalil <jkunn@carbonmedia.com>
2019-09-11 17:11:49 -0400

    evaluation date fix

--- app/Http/Controllers/Frontend/Huddle/HuddleController.php
+++ app/Http/Controllers/Frontend/Huddle/HuddleController.php
@@ -77,7 +77,7 @@ class HuddleController extends Controller
 
 
         $assessments = Assessment::whereIn('building_department_id', $huddle->careTeam->departments->pluck('id'))->where('created_at', '>', $last_huddle->created_at)->get();
-        $assessment_question_evaluations = QuestionEvaluation::whereIn('assessment_id', $assessments->pluck('id'))->orderBy('created_at', 'DESC')->get();
+        $assessment_question_evaluations = QuestionEvaluation::whereIn('assessment_id', $assessments->pluck('id'))->where('created_at', '>', $last_huddle->created_at)->orderBy('created_at', 'DESC')->get();
 
         return view('huddle.user.view', ['huddle' => $huddle, 'users' => $users, 'ilsm_assessments' => $ilsm_assessments, 'assessments' => $assessments, 'assessment_question_evaluations' => $assessment_question_evaluations]);
     }
